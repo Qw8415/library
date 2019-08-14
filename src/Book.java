64c6dc880ebb6adc541f3e830e8e7e717f3b0163
Qw8415 <g.szul00@gmail.com>
2019-08-14 21:39:38 +0200

    Second lesson

---
+++ src/Book.java
@@ -0,0 +1,8 @@
+class Book {
+    String title;
+    String author;
+    int relaseDate;
+    int pages;
+    String publisher;
+    String isbn;
+}
---
+++ src/Library.java
@@ -0,0 +1,24 @@
+class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.2";
+
+        Book book1 = new Book();
+
+        book1.title = "W pustyni i puszczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.relaseDate = 2010;
+        book1.pages = 296;
+        book1.publisher = "Greg";
+        book1.isbn = "9788393271890";
+
+        System.out.println(appName);
+        System.out.println("Dostępne książki:");
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.relaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
+
+    }
+}
