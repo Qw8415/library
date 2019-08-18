0441a15aa35277f18befb4438f5d4475002b9d46
Qw8415 <g.szul00@gmail.com>
2019-08-18 15:46:36 +0200

    Lection 17

--- src/qw8415/library/app/LibraryControl.java
+++ src/qw8415/library/app/LibraryControl.java
@@ -94,4 +94,33 @@ class LibraryControl {
         }
         System.out.println(Option.EXIT.toString());
     }
+
+    private enum Option {
+        EXIT(0, "Wyjdź"),
+        ADD_BOOK(1, "Dodaj książkę"),
+        ADD_MAGAZINE(2, "Dodaj magazyn"),
+        PRINT_BOOKS(3, "Dostępne książki"),
+        PRINT_MAGAZINES(4, "Dostępne magazyny");
+
+        private int value;
+        private String description;
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        static Option createFromInt(int option) throws NoSuchOptionException {
+            try {
+                return Option.values()[option];
+            } catch (IndexOutOfBoundsException e) {
+                throw new NoSuchOptionException("Brak opcji " + option);
+            }
+        }
+    }
 }
