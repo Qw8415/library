50366793ebb15a89d28da739c0680ddac2329220
Qw8415 <g.szul00@gmail.com>
2019-08-22 15:30:50 +0200

    Cosmetics

--- src/qw8415/library/app/LibraryApp.java
+++ src/qw8415/library/app/LibraryApp.java
@@ -1,7 +1,7 @@
 package qw8415.library.app;
 
 class LibraryApp {
-    final static String APP_NAME = "Biblioteka v2.0";
+    final static String APP_NAME = "Biblioteka v2.2";
 
     public static void main(String[] args) {
         LibraryControl controller = new LibraryControl();
